1789df24a84a3e12090af838b67bcde4692347ea
Mariano Manuel Torrado Sanchez <marianomtorrados@gmail.com>
2020-04-20 16:19:12 +0200

    GITIGNORE

--- gestionas/gestionBD.php
+++ gestionas/gestionBD.php
@@ -3,8 +3,8 @@
 function crearConexionBD()
 {
 	$host="oci:dbname=localhost/XE;charset=UTF8";
-	$usuario="CONEXION";
-	$password="123456";
+	$usuario="COENCAMMTS";
+	$password="mariano";
 
 	try{
 		/* Indicar que las sucesivas conexiones se puedan reutilizar */	
