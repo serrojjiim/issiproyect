94fc89da1c39936007c274ccfb9d4d8617f85235
Mariano Manuel Torrado Sanchez <marianomtorrados@gmail.com>
2020-04-25 18:09:35 +0200

    no va el borrar xd

--- gestionas/gestionarMaquina.php
+++ gestionas/gestionarMaquina.php
@@ -5,4 +5,22 @@ function consultarMaquinas($conexion) {
 	return $conexion->query($consulta);
 }
 
+function getMaquinaOid($conexion,$oid){
+	$consulta = "SELECT * FROM CLIENTE WHERE (MAQUINA.OID_MAQ = '$oid')";
+	$stmt = $conexion->prepare($consulta);
+	$stmt->execute();
+	return $stmt->fetch();
+}
+
+function borrarMaq($conexion,$oid){
+		try{
+	$consulta = "DELETE FROM MAQUINA WHERE (MAQUINA.OID_MAQ = '$oid')";
+	$stmt = $conexion->prepare($consulta);
+	$stmt->execute();
+	return "";
+		}catch(PDOException $e) {
+		return $e->getMessage();
+    }
+}
+
 ?>
